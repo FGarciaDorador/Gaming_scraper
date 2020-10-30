f71dabc4c13cc7fee233cbb7d17c4bfe1ecf40f4
Albert Alonso <aalonso@protonmail.com>
2020-10-30 21:16:59 +0100

    reorganize code
    * Move logic to gscrap dir.
    * Create a run.py file that handles the execution.

--- gscrap/selenium_steam.py
+++ gscrap/selenium_steam.py
@@ -1,32 +1,40 @@
 from selenium import webdriver
 import time
+import yalm
 
-class steam_scrapper ():
+class Scrapper:
+    def __init__(self):
+        with open('config.yml', 'r') as config_file:
+            config = yalm.load(config_file)
+            executable_path = config['webdriver']['geckodriver_path']
+            driver = config['webdriver']['browser']
+
+class SteamScrapper(Scrapper):
 
     def __init__(self):
-
+        super(self, SteamScrapper).__init__()
         self.driver = webdriver.Firefox(executable_path = r"C:\Users\Fernando\Desktop\geckodriver-v0.27.0-win64\geckodriver.exe")
         self.url = "https://store.steampowered.com/"
         self.driver.get(self.url)
 
 
     def price(self,item):
-        
+
         searchbox = self.driver.find_element_by_id("store_nav_search_term")
         searchbox.send_keys(item)
         #caja donde se ve la imagen, título y precio del juego
         suggestion_box = self.driver.find_element_by_id("search_suggestion_contents")
-        
+
         time.sleep(0.5) #necesita reposar para encontrar más info
         #hacemos try except para aquellos juegos que no se encuentren en steam o quizá estén mal escritos
         try:
             price = suggestion_box.find_element_by_class_name("match_price").text
         except:
             price = None
-        
+
         searchbox.clear()
         return price
-        
+
     def quit(self):
-        
+
         self.driver.quit()
---
+++ run.py
@@ -0,0 +1,5 @@
+from gscrap import IG_scrapper
+
+if __name__ == '__main__':
+    # TODO(albert): Detect website and pass it as argument
+    IG_scrapper.run()
