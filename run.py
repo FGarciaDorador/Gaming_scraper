d916a4c29071b611cb091f778f35b1fa7c485d40
Albert Alonso <aalonso@protonmail.com>
2020-11-05 21:10:59 +0100

    fixed imports

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
-from gscrap import IG_scrapper
+from gscrap import IG_scraping
 
 if __name__ == '__main__':
     # TODO(albert): Detect website and pass it as argument
-    IG_scrapper.run()
+    IG_scraping.run()
